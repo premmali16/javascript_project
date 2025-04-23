f0d1fcd52ced6f5660ed807a0a9c1daffebee502
premmali16 <premmali338@gmail.com>
2025-04-23 09:19:31 +0530

    Add files via upload

---
+++ javascript_project/project 6/stone/stone.js
@@ -0,0 +1,30 @@
+
+    function start(select1){
+
+
+        
+        let result=document.getElementById("result");
+        let computer_num=Math.random();
+        computer_num=parseInt(computer_num*10+1);
+        let computer_chosse="";
+        
+        if(computer_num >0 && computer_num <4){
+computer_chosse="stone";
+        }
+        
+        if(computer_num >3 && computer_num <7){
+computer_chosse="paper";
+        }
+        
+        if(computer_num >6 && computer_num <11){
+computer_chosse="scissors";
+        }
+      
+        
+       if(computer_chosse==select1){
+        result.innerHTML=`you chosse ${select1} and computer chosse ${computer_chosse}. you will be win `;
+       } 
+       else{
+        result.innerHTML=`you chosse ${select1} and computer chosse ${computer_chosse}. you will be loss`;
+       }
+    }
